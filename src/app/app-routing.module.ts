dc71d5eaaa0b93903fd2c7c35e1dae86873bafe6
jackie <bobbinnej@gmail.com>
2022-04-16 00:05:02 +0300

    Add routes and make them functional

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { RepositoryComponent } from './repository/repository.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'homepage', component : HomepageComponent},
+  {path: 'repository', component : RepositoryComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
