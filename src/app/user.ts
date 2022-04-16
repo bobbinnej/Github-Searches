becc61bce403e68242a7fcfd0c6d9529a7e65a36
jackie <bobbinnej@gmail.com>
2022-04-16 15:36:40 +0300

    place access key inside environment ts then inside gitignore file

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,16 @@
 export class User {
+
+    constructor(
+        public login:string,
+        public avatar_url:string,
+        public followers_url:string,
+        public following_url:string,
+        public repo_url:string,
+        public repos_url:string,
+        public fname_string:string,
+        public ulocation:string,
+        public  createdAt:Date
+
+        
+        ){}
 }
